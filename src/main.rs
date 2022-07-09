6fa8e212a7d9f71a092c7e08c00cb03b2641fa4a
Thi Dinh <vietthidinh2001@gmail.com>
2022-07-09 23:42:32 +0300

    fix: typo in target detail json file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![no_std] // don't link std library
-#![no_main] // disable all Rust-level entry points
+#![no_std] // don't link std library.
+#![no_main] // disable all Rust-level entry points.
 
 use core::panic::PanicInfo;
 
@@ -8,7 +8,9 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle] // disable name mangling to make sure the func name after compiled remains exactly `_start`
+#[no_mangle] // disable name mangling to make sure the func name after compiled remains exactly `_start`.
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default.
     loop {}
 }
